6c9a518fc47fe3b559f3c54f37702731ae775dd3
Shreyas Ashtamkar <Shreyu@programmer.net>
2021-05-13 14:06:51 +0530

    Clean Code

--- test2_Screen_capture.py
+++ test2_Screen_capture.py
@@ -3,11 +3,11 @@ import pyautogui
 import numpy as np
 
 def captureimage():
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
     while True:
         cap = pyautogui.screenshot()
         frame = np.array(cap)
-        # ret,frame = cap.read()
+        
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -15,5 +15,5 @@ def captureimage():
             break
         
 captureimage()
-# cap.release()
+
 cv2.destroyAllWindows()
